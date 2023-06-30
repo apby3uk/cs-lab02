954fe7393a11a523e927d98475d6bf503092cdc5
Alice (DegtyarevAS) <victor2017selvo@mail.ru>
2023-06-30 10:07:30 +0300

    code: добавлен вывод максимума a и b

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     cout << "A + B = " << a + b << '\n'
      << "A - B = " << a - b << '\n'
      << "A * B = " << a * b << '\n'
-     << "A / B = " << a / b << '\n';
+     << "A / B = " << a / b << '\n'
+     << "MAX = " << max(a,b) << '\n';
     return 0;
 }
